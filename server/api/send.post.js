f7fa2a061ffb1ddea1f6f75d683d2fed75cfa84f
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2022-08-09 14:34:55 +0300

    форма бриф

--- server/api/send.post.js
+++ server/api/send.post.js
@@ -1,29 +1,38 @@
 import nodemailer from 'nodemailer'
+import formidable from 'formidable'
 export default defineEventHandler(async (event) => {
     const body = await useBody(event)
-    console.log(body);
+    const form = formidable({ multiples: true });
+    form.parse(body, (err, fields, files) => {
+      console.log('fields: ', fields);
+      console.log('files: ', files);
+  });
 
-    const transporter = nodemailer.createTransport({
-        port: 465, // true for 465, false for other ports
-        host: "smtp.yandex.ru",
-        auth: {
-            user: '',
-            pass: '',
-        },
-        secure: true,
-    });
-    const mailData = {
-        from: '',
-        to: '',
-        subject: '',
-        text: ``,
-        html: ``,
-    }
-    transporter.sendMail(mailData, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-          console.log(info);
-        }
-    })
+    // console.log(body);
+
+    // const transporter = nodemailer.createTransport({
+    //     port: 465, // true for 465, false for other ports
+    //     host: "smtp.yandex.ru",
+    //     auth: {
+    //         user: '',
+    //         pass: '',
+    //     },
+    //     secure: true,
+    // });
+    // const mailData = {
+    //     from: '',
+    //     to: '',
+    //     subject: 'Бриф с сайта launch',
+    //     text: ``,
+    //     html: `<div>${body}</div>`,
+    // }
+    // transporter.sendMail(mailData, (error, info) => {
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //       console.log(info);
+    //     }
+    // })
+    // console.log(body);
+    // return { body }
 })
