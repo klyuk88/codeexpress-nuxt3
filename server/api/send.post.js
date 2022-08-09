44042117607ab59f146ae7413a8bcdbd1f336a9c
Alexey Kovalev <to.kovalev@gmail.com>
2022-08-09 15:27:11 +0300

    Внесены изменения

--- server/api/send.post.js
+++ server/api/send.post.js
@@ -1,38 +1,41 @@
-import nodemailer from 'nodemailer'
-import formidable from 'formidable'
+import nodemailer from 'nodemailer';
+import formidable from 'formidable';
+
 export default defineEventHandler(async (event) => {
-    const body = await useBody(event)
-    const form = formidable({ multiples: true });
-    form.parse(body, (err, fields, files) => {
-      console.log('fields: ', fields);
-      console.log('files: ', files);
+  const { req } = event;
+
+  const form = formidable({ multiples: true });
+
+  form.parse(req, (err, fields, files) => {
+    console.log('fields: ', fields);
+    console.log('files: ', files);
   });
 
-    // console.log(body);
+  // console.log(body);
 
-    // const transporter = nodemailer.createTransport({
-    //     port: 465, // true for 465, false for other ports
-    //     host: "smtp.yandex.ru",
-    //     auth: {
-    //         user: '',
-    //         pass: '',
-    //     },
-    //     secure: true,
-    // });
-    // const mailData = {
-    //     from: '',
-    //     to: '',
-    //     subject: 'Бриф с сайта launch',
-    //     text: ``,
-    //     html: `<div>${body}</div>`,
-    // }
-    // transporter.sendMail(mailData, (error, info) => {
-    //     if (error) {
-    //         console.log(error);
-    //     } else {
-    //       console.log(info);
-    //     }
-    // })
-    // console.log(body);
-    // return { body }
-})
+  // const transporter = nodemailer.createTransport({
+  //     port: 465, // true for 465, false for other ports
+  //     host: "smtp.yandex.ru",
+  //     auth: {
+  //         user: '',
+  //         pass: '',
+  //     },
+  //     secure: true,
+  // });
+  // const mailData = {
+  //     from: '',
+  //     to: '',
+  //     subject: 'Бриф с сайта launch',
+  //     text: ``,
+  //     html: `<div>${body}</div>`,
+  // }
+  // transporter.sendMail(mailData, (error, info) => {
+  //     if (error) {
+  //         console.log(error);
+  //     } else {
+  //       console.log(info);
+  //     }
+  // })
+  // console.log(body);
+  // return { body }
+});
