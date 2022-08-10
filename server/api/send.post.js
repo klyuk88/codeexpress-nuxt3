eb2fd431f29a3a9b0a011b939b54e073edba933c
Alexey Kovalev <to.kovalev@gmail.com>
2022-08-10 21:37:33 +0300

    Внесены изменения

--- server/api/send.post.js
+++ server/api/send.post.js
@@ -1,55 +1,99 @@
 import nodemailer from 'nodemailer';
 import formidable from 'formidable';
 
-export default defineEventHandler(async (event) => {
-    const {
-        req,
-    } = event;
+// Если будут еще эндпоинты советую вынести функции successHandler и errorHandler в отдельный файл с утилитами, а тут импортировать их
 
-    const form = formidable();
+const successHandler = (res, data = {}, message = 'ok', statusCode = 200) => {
+  res.statusCode = statusCode;
+
+  return {
+    data: {
+      status: false,
+      data,
+      message,
+    },
+  };
+};
+
+const errorHandler = (res, message = 'Неизвестная ошибка', statusCode = 400) => {
+  res.statusCode = statusCode;
+
+  return {
+    error: {
+      status: false,
+      message,
+    },
+  };
+};
+
+const getData = async (req) => {
+  const form = formidable();
+
+  return await new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
-        if (err) {
-          console.log(err);
-            return
-        } else {
-            let htmlBody = `<table style="width: 100%;">`
-            for (const key in fields) {
-                if (Object.hasOwnProperty.call(fields, key)) {
-                    htmlBody = htmlBody + `<tr style="background-color: #f8f8f8;">
+      if (err) {
+        reject(error);
+
+        return;
+      }
+
+      resolve({ fields, files });
+    });
+  });
+};
+
+export default defineEventHandler(async (event) => {
+  const { req, res } = event;
+
+  try {
+    const { fields, files } = await getData(req);
+
+    let htmlBody = `<table style="width: 100%;">`;
+
+    for (const key in fields) {
+      if (Object.hasOwnProperty.call(fields, key)) {
+        htmlBody =
+          htmlBody +
+          `<tr style="background-color: #f8f8f8;">
                 <td style="padding: 10px; border: #e9e9e9 1px solid;"><b>${key}</b></td>
                 <td style="padding: 10px; border: #e9e9e9 1px solid;">${fields[key]}</td>
-                </tr>`
-                }
-            }
-            htmlBody = htmlBody + `</table>`
+                </tr>`;
+      }
+    }
+    htmlBody = htmlBody + `</table>`;
 
-            const transporter = nodemailer.createTransport({
-                port: 465, // true for 465, false for other ports
-                host: "smtp.yandex.ru",
-                auth: {
-                    user: '',
-                    pass: '',
-                },
-                secure: true,
-            });
-            const mailData = {
-                from: 'no-replay@launchplus.ru',
-                to: '',
-                subject: 'Бриф с сайта launch',
-                text: ``,
-                html: htmlBody,
-                // attachments: attachments
-            }
-            transporter.sendMail(mailData, (error, info) => {
-                if (error) {
-                    return console.log(error);
-                } else {
-                    return console.log(info);
-                }
-            })
-
-
-        }
+    const transporter = nodemailer.createTransport({
+      port: 465, // true for 465, false for other ports
+      host: 'smtp.yandex.ru',
+      auth: {
+        user: '',
+        pass: '',
+      },
+      secure: true,
     });
 
+    const mailData = {
+      from: 'no-replay@launchplus.ru',
+      to: '',
+      subject: 'Бриф с сайта launch',
+      text: ``,
+      html: htmlBody,
+      attachments: [
+        {
+          filename: files.file.originalFilename,
+          path: files.file.filepath,
+        },
+      ],
+    };
+
+    try {
+      const result = await transporter.sendMail(mailData);
+
+      return successHandler(res, result);
+    } catch (error) {
+      return errorHandler(res, error.message);
+    }
+  } catch (error) {
+    return errorHandler(res, error.message);
+  }
 });
