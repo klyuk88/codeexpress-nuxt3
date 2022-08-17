a238a00ff1b2047249694b1df048ade1d20f860d
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2022-08-17 18:33:24 +0300

    prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'codeexpress',
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs'
+      script: '.output/server/index.mjs'
     }
   ]
 }
